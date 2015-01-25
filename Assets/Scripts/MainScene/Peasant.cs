e64e3ae67a8d692f1f0da82d94c641154b9f7da2
Nenad Stojanovikj <nekk1@live.com>
2015-01-25 02:20:53 +0100

    Rain movement, more models

---
+++ Assets/Scripts/MainScene/Peasant.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Peasant : MonoBehaviour {
+
+    public MeshRenderer leftSleeve;
+    public MeshRenderer rightSleeve;
+    public MeshRenderer body;
+
+	// Use this for initialization
+	void Start () {
+
+        float r = Random.Range(0, 1f);
+        float g = Random.Range(0, 1f);
+        float b = Random.Range(0, 1f);
+
+        leftSleeve.material.color = new Color(r, g, b);
+        rightSleeve.material.color = new Color(r, g, b);
+        body.material.color = new Color(r, g, b);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
